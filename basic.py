f810ae7877eae55548a438e1bdf5a098e6178425
azitechsolutions <azizul_official@yahoo.com>
2021-04-01 14:49:43 +0800

    update

--- basic.py
+++ basic.py
@@ -15,7 +15,7 @@ modbusClient.connect()
 
 
 StartAddressA = 256
-dataA = modbusClient.read_holdingregisters(StartAddressA,36)
+dataA = modbusClient.read_holdingregisters(StartAddressA,32)
 
 print(dataA)
 #print(dataB)
