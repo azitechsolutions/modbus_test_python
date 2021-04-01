15dcba04ddda44f093a39a6b1a9fd4c4d52622c3
azitechsolutions <azizul_official@yahoo.com>
2021-04-01 13:09:45 +0800

    update

--- reader.py
+++ reader.py
@@ -132,7 +132,7 @@ def DWORD32(register, startRegister, dataArray):
 StartAddressA = 3900
 
 dataA = modbusClient.read_holdingregisters(StartAddressA,2)
-prit(dataA)
+print(dataA)
 energy = 0
 
 activepowera = 0
