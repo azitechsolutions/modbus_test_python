f4325ae75b522c77935051522e70623221bdbff9
azitechsolutions <azizul_official@yahoo.com>
2021-04-01 14:36:59 +0800

    update

--- reader.py
+++ reader.py
@@ -129,95 +129,96 @@ def DWORD32(register, startRegister, dataArray):
     value = float(int(joinHex,16))
     return value
 
-StartAddress_A = 4000
+StartAddress_A = 256
 
-data_A = modbusClient.read_holdingregisters(StartAddress_A, 58)
-
-energy = UINT32_V2(4002,StartAddress_A,data_A)
-print("Energy Accumulate: {}".format(energy))
+data_A = modbusClient.read_holdingregisters(StartAddress_A, 52)
 print(data_A)
 
-activepowera = UINT16(4041,StartAddress_A,data_A)/1000
-activepowerb = UINT16(4043,StartAddress_A,data_A)/1000
-activepowerc = UINT16(4045,StartAddress_A,data_A)/1000
+# energy = UINT32_V2(4002,StartAddress_A,data_A)
+# print("Energy Accumulate: {}".format(energy))
+# print(data_A)
 
-apparentpowera = UINT16(4047,StartAddress_A,data_A)/1000
-apparentpowerb = UINT16(4049,StartAddress_A,data_A)/1000
-apparentpowerc = UINT16(4051,StartAddress_A,data_A)/1000
+# activepowera = UINT16(4041,StartAddress_A,data_A)/1000
+# activepowerb = UINT16(4043,StartAddress_A,data_A)/1000
+# activepowerc = UINT16(4045,StartAddress_A,data_A)/1000
 
-reactivepowera = UINT16(4053,StartAddress_A,data_A)/1000
-reactivepowerb = UINT16(4055,StartAddress_A,data_A)/1000
-reactivepowerc = UINT16(4057,StartAddress_A,data_A)/1000
+# apparentpowera = UINT16(4047,StartAddress_A,data_A)/1000
+# apparentpowerb = UINT16(4049,StartAddress_A,data_A)/1000
+# apparentpowerc = UINT16(4051,StartAddress_A,data_A)/1000
 
-voltageab = UINT16(4029,StartAddress_A,data_A)/10
-voltagebc = UINT16(4031,StartAddress_A,data_A)/10
-voltageca = UINT16(4033,StartAddress_A,data_A)/10
-voltagean = UINT16(4035,StartAddress_A,data_A)/10
-voltagebn = UINT16(4037,StartAddress_A,data_A)/10
-voltagecn = UINT16(4039,StartAddress_A,data_A)/10
-voltagell = 0
-voltagelnavg = 0
+# reactivepowera = UINT16(4053,StartAddress_A,data_A)/1000
+# reactivepowerb = UINT16(4055,StartAddress_A,data_A)/1000
+# reactivepowerc = UINT16(4057,StartAddress_A,data_A)/1000
 
-currenta = UINT16(4021,StartAddress_A,data_A)/100
-currentb = UINT16(4023,StartAddress_A,data_A)/100
-currentc = UINT16(4025,StartAddress_A,data_A)/100
+# voltageab = UINT16(4029,StartAddress_A,data_A)/10
+# voltagebc = UINT16(4031,StartAddress_A,data_A)/10
+# voltageca = UINT16(4033,StartAddress_A,data_A)/10
+# voltagean = UINT16(4035,StartAddress_A,data_A)/10
+# voltagebn = UINT16(4037,StartAddress_A,data_A)/10
+# voltagecn = UINT16(4039,StartAddress_A,data_A)/10
+# voltagell = 0
+# voltagelnavg = 0
 
-powerfactora = UINT16(4018,StartAddress_A,data_A)/1000
-powerfactorb = UINT16(4018,StartAddress_A,data_A)/1000
-powerfactorc = UINT16(4018,StartAddress_A,data_A)/1000
+# currenta = UINT16(4021,StartAddress_A,data_A)/100
+# currentb = UINT16(4023,StartAddress_A,data_A)/100
+# currentc = UINT16(4025,StartAddress_A,data_A)/100
 
-frequency = UINT16(4019,StartAddress_A,data_A)/100
+# powerfactora = UINT16(4018,StartAddress_A,data_A)/1000
+# powerfactorb = UINT16(4018,StartAddress_A,data_A)/1000
+# powerfactorc = UINT16(4018,StartAddress_A,data_A)/1000
 
-demandpowerlast = 0
-demandpowerpresent = 0
-demandpowerpredicted = 0 
-demandpowerpeak = 0
-demandcurrentlast = 0
-demandcurrentpresent = 0
-demandcurrentpredicted = 0
-demandcurrentpeak = 0
+# frequency = UINT16(4019,StartAddress_A,data_A)/100
 
-
-
-
-print("Energy: {}".format(energy))
-
-print("Active Power A: {}".format(activepowera))
-print("Active Power B: {}".format(activepowerb))
-print("Active Power B: {}".format(activepowerc))
-
-print("Apparent Power A: {}".format(apparentpowera))
-print("Apparent Power B: {}".format(apparentpowerb))
-print("Apparent Power B: {}".format(apparentpowerc))
-
-print("Reactive Power A: {}".format(reactivepowera))
-print("Reactive Power B: {}".format(reactivepowerb))
-print("Reactive Power B: {}".format(reactivepowerc))
-
-print("Voltage AB: {}".format(voltageab))
-print("Voltage BC: {}".format(voltagebc))
-print("Voltage CA: {}".format(voltageca))
-print("Voltage AN: {}".format(voltagean))
-print("Voltage BN: {}".format(voltagebn))
-print("Voltage CN: {}".format(voltagecn))
-print("Voltage LL: {}".format(voltagell))
-print("Voltage LNAvg: {}".format(voltagelnavg))
-
-print("Current A: {}".format(currenta))
-print("Current B: {}".format(currentb))
-print("Current C: {}".format(currentc))
-
-print("Powerfactor A: {}".format(powerfactora))
-print("Powerfactor B: {}".format(powerfactorb))
-print("Powerfactor C: {}".format(powerfactorc))
-
-print("Frequency: {}".format(frequency))
+# demandpowerlast = 0
+# demandpowerpresent = 0
+# demandpowerpredicted = 0 
+# demandpowerpeak = 0
+# demandcurrentlast = 0
+# demandcurrentpresent = 0
+# demandcurrentpredicted = 0
+# demandcurrentpeak = 0
 
 
-print("Demand Power Last: {}".format(demandpowerlast))
-print("Demand Power Present: {}".format(demandcurrentpresent))
-print("Demand Power Predicted: {}".format(demandpowerpredicted))
-print("Demand Power Peak: {}".format(demandpowerpeak))
+
+
+# print("Energy: {}".format(energy))
+
+# print("Active Power A: {}".format(activepowera))
+# print("Active Power B: {}".format(activepowerb))
+# print("Active Power B: {}".format(activepowerc))
+
+# print("Apparent Power A: {}".format(apparentpowera))
+# print("Apparent Power B: {}".format(apparentpowerb))
+# print("Apparent Power B: {}".format(apparentpowerc))
+
+# print("Reactive Power A: {}".format(reactivepowera))
+# print("Reactive Power B: {}".format(reactivepowerb))
+# print("Reactive Power B: {}".format(reactivepowerc))
+
+# print("Voltage AB: {}".format(voltageab))
+# print("Voltage BC: {}".format(voltagebc))
+# print("Voltage CA: {}".format(voltageca))
+# print("Voltage AN: {}".format(voltagean))
+# print("Voltage BN: {}".format(voltagebn))
+# print("Voltage CN: {}".format(voltagecn))
+# print("Voltage LL: {}".format(voltagell))
+# print("Voltage LNAvg: {}".format(voltagelnavg))
+
+# print("Current A: {}".format(currenta))
+# print("Current B: {}".format(currentb))
+# print("Current C: {}".format(currentc))
+
+# print("Powerfactor A: {}".format(powerfactora))
+# print("Powerfactor B: {}".format(powerfactorb))
+# print("Powerfactor C: {}".format(powerfactorc))
+
+# print("Frequency: {}".format(frequency))
+
+
+# print("Demand Power Last: {}".format(demandpowerlast))
+# print("Demand Power Present: {}".format(demandcurrentpresent))
+# print("Demand Power Predicted: {}".format(demandpowerpredicted))
+# print("Demand Power Peak: {}".format(demandpowerpeak))
 
 
 
