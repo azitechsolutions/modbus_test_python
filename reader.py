b5e3f5d4584802caaf964715fd599c114700b20e
azitechsolutions <azizul_official@yahoo.com>
2021-04-01 15:23:37 +0800

    update

--- reader.py
+++ reader.py
@@ -97,7 +97,6 @@ def INT32(register, startRegister, dataArray):
 def INT32_V2_Inverse(register, startRegister, dataArray):
     low = dataArray[register-startRegister]
     high = dataArray[(register-startRegister)+1]  
-    print("high: {}   low: {}".format(high,low))
     low_hex = "%02x" % int(low)
     high_hex = "%02x" % int(high)
     for i in range(len(low_hex),4):
