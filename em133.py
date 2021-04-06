2644afd9bab604d2446a90388a2db0b60c1c0351
azitechsolutions <azizul_official@yahoo.com>
2021-04-06 13:51:39 +0800

    update

--- em133.py
+++ em133.py
@@ -206,7 +206,7 @@ voltagecn = UINT32_V2_Inverse(13312+4, StartAddress_A, data_A)
 voltagell = 0
 voltagelnavg = 0
 
-currenta = UINT16(StartAddress_E+3, StartAddress_E, data_E)
+currenta = UINT16(StartAddress_E+3, StartAddress_E, data_E)*((CTPrimary*2)-0)/(9999-0)+0
 currentb = UINT32_V2_Inverse(13312+6, StartAddress_A, data_A)*((CTPrimary*2)-0)/(9999-0)+0
 currentc = UINT32_V2_Inverse(13312+10, StartAddress_A, data_A)*((CTPrimary*2)-0)/(9999-0)+0
 
