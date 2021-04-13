cf4adb9cb3f57fc785cca7cb4b0d42f57e93195c
Azizul Othman <>
2021-04-13 17:25:10 +0800

    update

---
+++ mdm3100.py
@@ -0,0 +1,262 @@
+from pymodule.easymodbus.modbusClient import *
+import ast
+import struct
+import codecs
+
+
+
+modbusClient = ModbusClient("/dev/ttyS0")
+modbusClient.parity = Parity.none
+modbusClient.baudrate = 9600
+modbusClient.stopbits = Stopbits.one
+modbusClient.unitidentifier = 6
+modbusClient.connect()
+
+def int32bits(hexstr):
+    dec = int(hexstr,16)
+    value = dec
+    if value & (1 << (32-1)):
+        value -= 1 << 32
+    return value
+def int16bits(hexstr):
+    value = int(hexstr,16)
+    if value & (1 << (16-1)):
+        value -= 1 << 16
+    return value
+
+def UINT32toFloat(register, startRegister, dataArray):
+    hex1 = "%02x" % int(dataArray[(register - startRegister)-1])
+    hex2 = "%02x" % int(dataArray[register - startRegister])
+
+    for i in range(len(hex1),4):
+        hex1 = "0{}".format(hex1)
+    for i in range(len(hex2),4):
+        hex2 = "0{}".format(hex2)
+
+    joinHex = "{}{}".format(hex1,hex2)
+
+    flotingPoint = struct.unpack('!f', codecs.decode(joinHex, 'hex'))[0]
+    
+    result = ("%.2f" % flotingPoint)
+    
+    return round(float(result), 2)
+def UINT32toFloat_V2(register, startRegister, dataArray):
+    hex1 = "%02x" % int(dataArray[(register - startRegister)])
+    hex2 = "%02x" % int(dataArray[(register - startRegister)+1])
+
+    for i in range(len(hex1),4):
+        hex1 = "0{}".format(hex1)
+    for i in range(len(hex2),4):
+        hex2 = "0{}".format(hex2)
+
+    joinHex = "{}{}".format(hex1,hex2)
+
+    flotingPoint = struct.unpack('!f', codecs.decode(joinHex, 'hex'))[0]
+    
+    result = ("%.2f" % flotingPoint)
+    
+    return round(float(result), 2)
+def UINT32(register, startRegister, dataArray):
+    low = dataArray[register-startRegister]
+    high = dataArray[(register-startRegister)-1] 
+
+    # print("Data Array: {}    Low:{}   High: {}".format(dataArray,low,high))
+    val = (high * 65536) + low 
+    result = ("%.2f" % val)
+    return round(float(result), 2)
+def UINT32_V2(register, startRegister, dataArray):
+    high = dataArray[register-startRegister]
+    low = dataArray[(register-startRegister)+1] 
+
+    # print("Data Array: {}    Low:{}   High: {}".format(dataArray,low,high))
+    val = (high * 65536) + low 
+    result = ("%.2f" % val)
+    return round(float(result), 2)
+def UINT32_V3(register, startRegister, dataArray):
+    high = dataArray[register-startRegister]
+    mid = dataArray[(register-startRegister)+1] 
+    low = dataArray[(register-startRegister)+2] 
+
+    # print("Data Array: {}    Low:{}   High: {}".format(dataArray,low,high))
+    val = ((high * 65536 * 65536) + (mid*65536)+ low )
+    result = ("%.2f" % val)
+    return round(float(result), 2)
+def INT32(register, startRegister, dataArray):
+    low = dataArray[register-startRegister]
+    high = dataArray[(register-startRegister)-1]  
+    low_hex = "%02x" % int(low)
+    high_hex = "%02x" % int(high)
+    for i in range(len(low_hex),4):
+        low_hex = "0{}".format(low_hex)
+    for i in range(len(high_hex),4):
+        high_hex = "0{}".format(high_hex)
+
+    joinHex = "{}{}".format(high_hex,low_hex)
+    result = int32bits(joinHex)
+    return round(float(result), 2)
+
+def UINT32_V2_Inverse(register, startRegister, dataArray):
+    low = dataArray[register-startRegister]
+    high = dataArray[(register-startRegister)+1] 
+
+    # print("Data Array: {}    Low:{}   High: {}".format(dataArray,low,high))
+    val = (high * 65536) + low 
+    result = ("%.2f" % val)
+    return round(float(result), 2)
+
+def INT32_V2_Inverse(register, startRegister, dataArray):
+    low = dataArray[register-startRegister]
+    high = dataArray[(register-startRegister)+1]  
+    low_hex = "%02x" % int(low)
+    high_hex = "%02x" % int(high)
+    for i in range(len(low_hex),4):
+        low_hex = "0{}".format(low_hex)
+    for i in range(len(high_hex),4):
+        high_hex = "0{}".format(high_hex)
+
+    joinHex = "{}{}".format(high_hex,low_hex)
+    result = int32bits(joinHex)
+    return round(float(result), 2)
+def INT32_V2(register, startRegister, dataArray):
+    high = dataArray[register-startRegister]
+    low = dataArray[(register-startRegister)+1]  
+    low_hex = "%02x" % int(low)
+    high_hex = "%02x" % int(high)
+    for i in range(len(low_hex),4):
+        low_hex = "0{}".format(low_hex)
+    for i in range(len(high_hex),4):
+        high_hex = "0{}".format(high_hex)
+
+    joinHex = "{}{}".format(high_hex,low_hex)
+    result = int32bits(joinHex)
+    return round(float(result), 2)
+def UINT16(register, startRegister, dataArray):
+    val = dataArray[register-startRegister]
+    result = ("%.2f" % val)
+    return round(float(result), 2)
+def INT16(register, startRegister, dataArray):
+    dec = dataArray[register-startRegister]
+    hex_val = hex(dec)
+    result = int16bits(hex_val)
+    return round(float(result), 2)
+def DWORD32(register, startRegister, dataArray):
+    hex_low = "%02x" % int(dataArray[register - startRegister])
+    hex_high = "%02x" % int(dataArray[(register - startRegister)+1])
+
+    for i in range(len(hex_low),4):
+        hex_low = "0{}".format(hex_low)
+    for i in range(len(hex_high),4):
+        hex_high = "0{}".format(hex_high)
+
+    joinHex = "{}{}".format(hex_low,hex_high)
+    value = float(int(joinHex,16))
+    return value
+
+########################################################################################################
+
+
+# Read modbus
+rangea = 6216
+rangeb = 6656
+
+dataA = modbusClient.read_holdingregisters(rangea, 116)
+dataB = modbusClient.read_holdingregisters(rangeb, 16)
+
+
+energy = (UINT32_V2(6656, rangeb,dataB)+(UINT32_V2(6668, rangeb,dataB)/100000000))*1000
+# print("Energy Accumulate: {}".format(energy))
+
+activepowera = UINT32_V3(6252,rangea,dataA)/1000
+activepowerb = UINT32_V3(6255,rangea,dataA)/1000
+activepowerc = UINT32_V3(6258,rangea,dataA)/1000
+
+apparentpowera = UINT32_V3(6240,rangea,dataA)/1000
+apparentpowerb = UINT32_V3(6243,rangea,dataA)/1000
+apparentpowerc = UINT32_V3(6246,rangea,dataA)/1000
+
+reactivepowera = UINT32_V3(6264,rangea,dataA)/1000
+reactivepowerb = UINT32_V3(6267,rangea,dataA)/1000
+reactivepowerc = UINT32_V3(6270,rangea,dataA)/1000
+
+voltageab = UINT32_V2(6224,rangea,dataA)/10
+voltagebc = UINT32_V2(6228,rangea,dataA)/10
+voltageca = UINT32_V2(6226,rangea,dataA)/10
+voltagean = UINT32_V2(6216,rangea,dataA)/10
+voltagebn = UINT32_V2(6218,rangea,dataA)/10
+voltagecn = UINT32_V2(6220,rangea,dataA)/10
+voltagell = UINT32_V2(6230,rangea,dataA)/10
+voltagelnavg = UINT32_V2(6222,rangea,dataA)/10
+
+currenta = UINT32_V2(6232,rangea,dataA)/1000
+currentb = UINT32_V2(6234,rangea,dataA)/1000
+currentc = UINT32_V2(6236,rangea,dataA)/1000
+
+powerfactora = dataA[6276-rangea]/1000
+powerfactorb = dataA[6277-rangea]/1000
+powerfactorc = dataA[6278-rangea]/1000
+
+frequency = dataA[6280-rangea]/100
+
+demandpowerlast = 0
+demandpowerpresent = 0
+demandpowerpredicted = 0 
+demandpowerpeak = 0
+demandcurrentlast = 0
+demandcurrentpresent = 0
+demandcurrentpredicted = 0
+demandcurrentpeak = 0
+
+########################################################################################################
+
+print("CT1: {}".format(CT1))
+print("CT2: {}".format(CT2))
+print("PT1: {}".format(PT1))
+print("PT2: {}".format(PT2))
+
+
+print("Energy: {}".format(energy))
+
+print("Active Power A: {}".format(activepowera))
+print("Active Power B: {}".format(activepowerb))
+print("Active Power B: {}".format(activepowerc))
+
+print("Apparent Power A: {}".format(apparentpowera))
+print("Apparent Power B: {}".format(apparentpowerb))
+print("Apparent Power B: {}".format(apparentpowerc))
+
+print("Reactive Power A: {}".format(reactivepowera))
+print("Reactive Power B: {}".format(reactivepowerb))
+print("Reactive Power B: {}".format(reactivepowerc))
+
+print("Voltage AB: {}".format(voltageab))
+print("Voltage BC: {}".format(voltagebc))
+print("Voltage CA: {}".format(voltageca))
+print("Voltage AN: {}".format(voltagean))
+print("Voltage BN: {}".format(voltagebn))
+print("Voltage CN: {}".format(voltagecn))
+print("Voltage LL: {}".format(voltagell))
+print("Voltage LNAvg: {}".format(voltagelnavg))
+
+print("Current A: {}".format(currenta))
+print("Current B: {}".format(currentb))
+print("Current C: {}".format(currentc))
+
+print("Powerfactor A: {}".format(powerfactora))
+print("Powerfactor B: {}".format(powerfactorb))
+print("Powerfactor C: {}".format(powerfactorc))
+
+print("Frequency: {}".format(frequency))
+
+
+print("Demand Power Last: {}".format(demandpowerlast))
+print("Demand Power Present: {}".format(demandcurrentpresent))
+print("Demand Power Predicted: {}".format(demandpowerpredicted))
+print("Demand Power Peak: {}".format(demandpowerpeak))
+
+
+
+
+
+
+
+
