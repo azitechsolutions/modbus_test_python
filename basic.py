f08143a9dd1ae7c364284fdc07b4aee75b4ec1ae
Azizul Othman <>
2021-04-20 16:43:08 +0800

    update

--- basic.py
+++ basic.py
@@ -10,7 +10,7 @@ modbusClient = ModbusClient("/dev/ttyS0")
 modbusClient.parity = Parity.none
 modbusClient.baudrate = 9600
 modbusClient.stopbits = Stopbits.one
-modbusClient.unitidentifier = 1
+modbusClient.unitidentifier = 13
 modbusClient.connect()
 
 
